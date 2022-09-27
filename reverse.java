ace8711a2e45d9470f95bf78f70020057eebc502
Abhishek Chauhan <91529395+Abhishek-Chauhan-365@users.noreply.github.com>
2022-09-28 04:45:26 +0530

    Update reverse.java

--- reverse.java
+++ reverse.java
@@ -1,13 +1,30 @@
-package hello;
-
-import org.joda.time.LocalTime;
-
-public class HelloWorld {
-    public static void main(String[] args) {
-      LocalTime currentTime = new LocalTime();
-		  System.out.println("The current local time is: " + currentTime);
-
-        Greeter greeter = new Greeter();
-        System.out.println(greeter.sayHello());
+pipeline {
+    agent any
+    tools {
+        maven "MAVEN"
+        jdk "JDK"
     }
+    stages {
+        stage('Initialize'){
+            steps{
+                echo "PATH = ${M2_HOME}/bin:${PATH}"
+                echo "M2_HOME = /opt/maven"
+            }
+        }
+        stage('Build') {
+            steps {
+                dir("/var/lib/jenkins/workspace/demopipelinetask/my-app") {
+                sh 'mvn -B -DskipTests clean package'
+                }
+            }
+        }
+     }
+    post {
+       always {
+          junit(
+        allowEmptyResults: true,
+        testResults: '*/test-reports/.xml'
+      )
+      }
+   }
 }

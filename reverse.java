92d168480a61525c30188e59a4a059d676edd158
Abhishek Chauhan <91529395+Abhishek-Chauhan-365@users.noreply.github.com>
2022-09-28 04:40:25 +0530

    Update reverse.java

--- reverse.java
+++ reverse.java
@@ -1,16 +1,13 @@
-public class ReverseArray {  
-    public static void main(String[] args) {  
-        //Initialize array  
-        int [] arr = new int [] {1, 2, 3, 4, 5};  
-        System.out.println("Original array: ");  
-        for (int i = 0; i < arr.length; i++) {  
-            System.out.print(arr[i] + " ");  
-        }  
-        System.out.println();  
-        System.out.println("Array in reverse order: ");  
-        //Loop through the array in reverse order  
-        for (int i = arr.length-1; i >= 0; i--) {  
-            System.out.print(arr[i] + " ");  
-        }  
-    }  
-} 
+package hello;
+
+import org.joda.time.LocalTime;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+      LocalTime currentTime = new LocalTime();
+		  System.out.println("The current local time is: " + currentTime);
+
+        Greeter greeter = new Greeter();
+        System.out.println(greeter.sayHello());
+    }
+}
